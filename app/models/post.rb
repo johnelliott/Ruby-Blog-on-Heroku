9208a03b2f19c5146a692cbdaa9fe602b15fa652
johnelliott <johnelliott703@gmail.com>
2014-10-16 19:48:26 -0400

    switch true/false from demonstrating to a classmate to make drafts and posts contents appear as designated

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  scope :draft, -> { where(draft_status: false)}
-  scope :published, -> { where(draft_status: true)}
-  scope :search, ->(q) { where('body LIKE ? OR subject LIKE ?', params[:q], params[:q]).published}
+  scope :draft, -> { where(draft_status: true)}
+  scope :published, -> { where(draft_status: false)}
+  scope :search, ->(q) { where('body ILIKE ? OR subject ILIKE ?', params[:q], params[:q]).published}
   validates :body, presence: true
 end
