b7723b68738926effa84697dca3cc267cc2d9a34
johnelliott <johnelliott703@gmail.com>
2014-10-16 19:30:44 -0400

    add search method to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   scope :draft, -> { where(draft_status: false)}
   scope :published, -> { where(draft_status: true)}
+  scope :search, -> { where('body OR subject LIKE ?', params[:q]).published}
   validates :body, presence: true
 end
