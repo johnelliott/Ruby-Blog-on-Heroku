f04288903b54b7def523aef8db211699b371a48d
johnelliott <johnelliott703@gmail.com>
2014-10-17 10:26:43 -0400

    refactor during class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,14 @@
 class Post < ActiveRecord::Base
   scope :draft, -> { where(draft_status: true)}
   scope :published, -> { where(draft_status: false)}
-  scope :search, -> (q) { where('body LIKE ? OR subject LIKE ?', "%#{q}%", "%#{q}%")}
+  scope :search, -> (search_query) { where('body LIKE ? OR subject LIKE ?', "%#{search_query}%", "%#{search_query}%") }
   validates :body, presence: true
+
+  # def self.search(foo)
+  #   where('body LIKE ? OR subject LIKE ?', "%#{foo}%", "%#{foo}%")
+  # end
+
+  # def self.published
+  #   where(draft_status: false)
+  # end
 end
