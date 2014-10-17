6eb2301685ad97014d3549e2a3df15a4f17f8c4a
johnelliott <johnelliott703@gmail.com>
2014-10-16 20:21:08 -0400

    restore route to call search action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
   get 'drafts', to: 'posts#drafts'
+  get '/posts/search', to: 'posts#search'
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
