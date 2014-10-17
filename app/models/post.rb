f926b63d2da89838b9efdf1dd67cfc110aedcd4d
johnelliott <johnelliott703@gmail.com>
2014-10-16 20:15:20 -0400

    fix single quotes messing up SQL query

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   scope :draft, -> { where(draft_status: true)}
   scope :published, -> { where(draft_status: false)}
-  scope :search, ->(q) { where('body ILIKE ? OR subject ILIKE ?', params[:q], params[:q]).published}
+  scope :search, -> (q) { where('body LIKE ? OR subject LIKE ?', "%#{q}%", "%#{q}%")}
   validates :body, presence: true
 end
