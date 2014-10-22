50e68c71c22b60c0b927eac7f17f7f97de73e6ea
johnelliott <johnelliott703@gmail.com>
2014-10-21 23:31:42 -0400

    add routes and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
-
+  resources :users
+  # create users route, and point it to the new method in users controller 
+  get '/signup', to: 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
